0f29a14117f1e931e971715a86b86f49e35e680d
hiroraba <xornelius0313@gmail.com>
2017-11-29 23:31:11 +0900

    Initial Commit

---
+++ swift-httpdate/swift_httpdate.h
@@ -0,0 +1,19 @@
+//
+//  swift_httpdate.h
+//  swift-httpdate
+//
+//  Created by matsuohiroki on 2017/11/29.
+//  Copyright © 2017年 matsuohiroki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for swift_httpdate.
+FOUNDATION_EXPORT double swift_httpdateVersionNumber;
+
+//! Project version string for swift_httpdate.
+FOUNDATION_EXPORT const unsigned char swift_httpdateVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <swift_httpdate/PublicHeader.h>
+
+
